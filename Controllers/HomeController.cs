7eead3d855f3813c12337362447041b175bac803
Alex202200037 <127303231+Alex202200037@users.noreply.github.com>
2025-04-15 21:37:51 +0100

    Main

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Página "About Us"
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+
+    // Página "Properties"
+    public IActionResult Properties()
+    {
+        return View();
+    }
+
+    // Página "Services"
+    public IActionResult Services()
+    {
+        return View();
+    }
+
+    // Página "Contact Us"
+    public IActionResult ContactUs()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ImoSphere.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
